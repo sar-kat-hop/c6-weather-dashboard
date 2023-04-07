f24d32d1b9650806680333d341a4570b997f6c9c
Sarah <sarah.k.hopper@gmail.com>
2023-04-07 02:01:20 -0700

    continue trying to fix api key throwing 401 error in console

--- Assets/TEST/script.js
+++ Assets/TEST/script.js
@@ -4,7 +4,8 @@ var input = document.getElementById("city-input");
 var weatherDiv = document.getElementById("weather");
 
 function getCoordinates(city) {
-    var GEO_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${myKey}`;
+    var GEO_URL = "http://api.openweathermap.org/geo/1.0/direct?q=" + `${city}` + "&limit=1&appid=" + `${myKey}`;
+
 
     return fetch(GEO_URL)
         .then(function(response) {
@@ -25,7 +26,7 @@ function getCoordinates(city) {
 };
 
 function getCurrentWeather(lat, lon) {
-    var URL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${myKey}`;
+    var URL = "https://api.openweathermap.org/data/2.5/weather?lat=" + `${lat}` + "&lon=" + `${lon}` + "&appid=" + `${myKey}`;
 
     return fetch(URL)
         .then(function(response) {
