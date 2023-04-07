9db619d81f9ac41305469f66acb8b2c7a3ddb7e8
Sarah <sarah.k.hopper@gmail.com>
2023-04-07 16:28:59 -0700

    start refactor of script.js, comment out test code

--- Assets/testScript.js
+++ Assets/testScript.js
@@ -1,10 +1,11 @@
 var myKey = "24d6dabc28b4faa4bf2a0df1923872d5";
 var form = document.getElementById("search-form");
 var input = document.getElementById("city-input");
-// var weatherDiv = document.getElementById("weather");
+var weatherDiv = document.getElementById("weather");
 
 function getCoordinates(city) {
     var GEO_URL = "http://api.openweathermap.org/geo/1.0/direct?q=" + `${city}` + "&limit=1&appid=" + `${myKey}`;
+
 
     return fetch(GEO_URL)
         .then(function(response) {
@@ -32,7 +33,7 @@ function getCurrentWeather(lat, lon) {
             if(response.ok) {
                 return response.json();
             } else {
-                throw Error("Couldn't fetch current weather.");
+                throw Error("Couldn't fetch weather.");
             }
         })
         .then(function(data) {
@@ -44,51 +45,17 @@ function getCurrentWeather(lat, lon) {
         });
 };
 
-function get5DayForecast(lat, lon) {
-    var URL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + `${lat}` + "&lon=" + `${lon}` + "&appid=" + `${myKey}` + "&units=imperial";
-
-    return fetch(URL)
-        .then(function(response) {
-            if(response.ok) {
-                return response.json();
-            } else {
-                throw Error("Couldn't fetch 5-day forecast.");
-            }
-        })
-        .then(function(data) {
-            var forecast = [];
-
-            for (let i = 0; i < data.length; i += 8) {  //OpenWeather returns forecast data for every 3 hours. i += 8 skips over 8 items it'll return for the 5-day forecast so we only get the by-day info we're looking for. This loop adds 8 to i every iteration, processingly only for every 24 hours (8*3 = 24).
-                var forecastDay = {
-                    date: data.list[i].dt_txt,
-                    temp: data.list[i].main.temp,
-                    wind: data.list[i].wind.speed,
-                    humidity: data.list[i].main.humidity,
-                };
-                forecast.push(forecastDay);
-            }
-            return forecast;
-        });
-}
-
 form.addEventListener("submit", function(event) {
     event.preventDefault();
 
     var city = input.value;
-    
-    if (!city) {
-        return Error("No city entered or found.");
-    }
 
     getCoordinates(city)
         .then(function(coords) {
-            return Promise.all([getCurrentWeather(coords.lat, coords.lon), get5DayForecast(lat, lon)]);
+            return getCurrentWeather(coords.lat, coords.lon);
         })
         .then(function(data) {
             var {temp, humidity, wind } = data;
-
-            var 
-            // weatherDiv.innerHTML = `The temperature in ${city} is ${temp}°F. Humidity is at ${humidity}%. Wind speed is ${wind}mph.`;
-
+            weatherDiv.innerHTML = `The temperature in ${city} is ${temp}°F. Humidity is at ${humidity}%. Wind speed is ${wind}mph.`;
         });
     });
