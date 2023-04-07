34eee46ab7fe3aa40bb10445ee258edccf9b62f5
Sarah <sarah.k.hopper@gmail.com>
2023-04-07 16:58:09 -0700

    move vars for html els within scope of event listener fxn

--- Assets/TEST/script.js
+++ Assets/TEST/script.js
@@ -3,17 +3,6 @@ var form = document.getElementById("search-form");
 var input = document.getElementById("city-input");
 // var weatherDiv = document.getElementById("weather");
 
-//vars for creating els and appending weather info to page:
-var card = document.createElement("div");
-var cardHead = document.createElement("h4");
-var cardBody = document.createElement("div");
-
-var tempEl = document.createElement("p");
-var windEl = document.createElement("p");
-var humidEl = document.createElement("p");
-var imgEl = document.createElement("img");
-
-
 function getCoordinates(city) {
     var GEO_URL = "http://api.openweathermap.org/geo/1.0/direct?q=" + `${city}` + "&limit=1&appid=" + `${myKey}`;
 
@@ -93,22 +82,35 @@ form.addEventListener("submit", function(event) {
 
     getCoordinates(city)
         .then(function(coords) {
-            // return Promise.all([getCurrentWeather(coords.lat, coords.lon), get5DayForecast(lat, lon)]);
-            return getCurrentWeather(coords.lat, coords.lon);
+            // return getCurrentWeather(coords.lat, coords.lon);
+            return Promise.all([getCurrentWeather(coords.lat, coords.lon), get5DayForecast(lat, lon)]);
         })
         .then(function(data) {
-            var {temp, humidity, wind } = data;
+            // var {temp, humidity, wind } = data;
+            var currentWeather = data[0];
+            var forecast = data[1];
 
             var currentWeatherDiv = document.getElementById("current-weather");
             var currentWeatherHead = document.getElementById("current-header");
+            var forecastHeader = document.getElementById("forecast-header"); //to append heading when rendering forecast data
+            var forecastDiv = document.getElementById("5day-forecast");
 
-            
+            var card = document.createElement("div");
+            var cardHead = document.createElement("h4");
+            var cardBody = document.createElement("div");
+
+            var tempEl = document.createElement("p");
+            var windEl = document.createElement("p");
+            var humidEl = document.createElement("p");
+            var imgEl = document.createElement("img");
+
             // weatherDiv.innerHTML = `The temperature in ${city} is ${temp}°F. Humidity is at ${humidity}%. Wind speed is ${wind}mph.`;
 
+        })
+        .catch(function(error) {
+            console.log("Error:" + error);
         });
     });
 
-    var forecastHeader = document.getElementById("forecast-header"); //to append heading when rendering forecast data
-    var forecastDiv = document.getElementById("5day-forecast");
 
 
